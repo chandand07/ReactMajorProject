3cb35a4ee62f592b81eddc95d5c925f17dd92303
chandand07 <dhingrac069@gmail.com>
2024-06-17 16:57:29 +0530

    home-ui-responsive

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React Real Estate UI Design
+
+This commit includes the home page ui and the routes layout , with full responsiveness
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import "./layout.scss"
 import "./components/navbar/Navbar"
 import Navbar from "./components/navbar/Navbar"
+import HomePage from "./routes/homePage/homePage"
+import {createBrowserRouter , RouterProvider , Route , Link} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:(
+      <HomePage/>
+    )
+    },
+    {
+      path: "/list",
+      element: <ListPage/>
+    }
+  ]);
   return (
-    <div className="layout">
-      <Navbar/>
-    </div>
-  )
+    // <div className="layout">
+    //   <div className="navbaar">
+    //   <Navbar/>
+    //   </div>
+    //   <div className="content">
+    //   <HomePage/>
+    //   </div>
+    // </div>
+    <RouterProvider router={router}/>
+  );
 }
 
 export default App
